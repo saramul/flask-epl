654f814d50d16f929f3d5b0927142d55b32b7195
saramul <sarasurius@gmail.com>
2026-02-13 10:16:06 +0700

    make blueprint

--- epl/__init__.py
+++ epl/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from epl.extensions import db, migrate
+from epl.core.routes import core_bp
+from epl.clubs.routes import club_bp
+from epl.players.routes import player_bp
+# from epl.models import Club, Player
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///epl.sqlite'
-app.secret_key = b'hggytdfyrdsrtrytyio'
+def create_app():
+  app = Flask(__name__)
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/epl_s03_db'
+  app.secret_key = b'hggytdfyrdsrtrytyio'
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+  db.init_app(app)
+  migrate.init_app(app, db)
 
-from epl import routes, models
+  app.register_blueprint(core_bp, url_prefix='/')
+  app.register_blueprint(club_bp, url_prefix='/clubs')
+  app.register_blueprint(player_bp, url_prefix='/players')
+
+  return app
